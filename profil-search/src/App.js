3f79fd7205fcda6b5e292127e0ec5c4b425cb550
Morgane <farez.morgane@gmail.com>
2026-02-06 17:16:15 +0100

    feat: Initialize react project

---
+++ profil-search/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello, world !</h1>
+      <p>Ceci est mon premier projet React.</p>
+    </div>
+  );
+}
+
+export default App;
+
